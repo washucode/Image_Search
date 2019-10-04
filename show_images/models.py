a52b89ffcde194c47d194682f8affb10fdc4b061
washucode <estherw8525@gmail.com>
2019-10-04 14:43:15 +0300

    make Category model

--- show_images/models.py
+++ show_images/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+
+
 # Create your models here.
