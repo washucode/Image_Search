58fdff6675805a2d19043c265c44f19a5e857a8e
washucode <estherw8525@gmail.com>
2019-10-04 14:45:54 +0300

    add test for model Category

--- show_images/tests.py
+++ show_images/tests.py
@@ -1,1 +1,12 @@
 from django.test import TestCase
+from .models import Category
+
+
+class CategoryTestCase(TestCase):
+
+    # setup
+    def setUp(self):
+        self.cat1= Category(name='goodfood')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.cat1,Category))
