7fb3b4660dc010ae9b3c501a23adc7bb7a7bfc93
washucode <estherw8525@gmail.com>
2019-10-07 01:56:11 +0300

    register table in admin.py

--- show_images/admin.py
+++ show_images/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Image,location,Category
 # Register your models here.
+
+admin.site.register(Image)
+admin.site.register(location)
+admin.site.register(Category)
