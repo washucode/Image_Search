3bbf7543e818963688f18f0cfebe586bd11454a6
washucode <estherw8525@gmail.com>
2019-10-07 02:00:25 +0300

    fix bug

--- show_images/models.py
+++ show_images/models.py
@@ -35,18 +35,18 @@ class Image(models.Model):
         id_length = len(category_id)
         image_s = []
         for ids in range(id_length):
-            found_images = cls.objects.filter(image_category_in=[category_id[ids].ids])
+            found_images = cls.objects.filter(image_category_in=[category_id[ids].id])
             image_s.append(found_images)
         return image_s
 
-        
+
      @classmethod
      def find_by_location(cls,locationterm):
         location_id = location.objects.filter(location_name__icontains=locationterm)
         id_length = len(location_id)
         image_s = []
         for ids in range(id_length):
-            found_images = cls.objects.filter(location_taken_in=[location_id[ids].ids])
+            found_images = cls.objects.filter(location_taken_in=[location_id[ids].id])
             image_s.append(found_images)
         return image_s
 
