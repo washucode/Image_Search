6247b877658dff1d6e1933db0249e081c2806937
washucode <estherw8525@gmail.com>
2019-10-07 01:13:36 +0300

    add functions to facilitate displaying of images according to search term

--- show_images/views.py
+++ show_images/views.py
@@ -6,6 +6,26 @@ from .models import Category,location,Image
 def home(request):
    
     images = Image.allphotos()
-    return render(request, 'allphotos.html')
+    return render(request, 'allphotos.html',{'images':images})
 
-    
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        
+        
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+# def nairobi(request):
+#     locationterm = 'nairobi'
+#     location_images = Image.find_by_location(locationterm)
+#     return render(request,'nairobi.html',{'images':location_images})
+
+
