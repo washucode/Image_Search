3b4dd52ee8e53e9731db0d2841bfa9e6ad187c88
washucode <estherw8525@gmail.com>
2019-10-07 12:51:27 +0300

    final styling

--- show_images/urls.py
+++ show_images/urls.py
@@ -6,12 +6,7 @@ from . import views
 urlpatterns=[
     url('^$',views.home, name="allphotos"),
     url('^search/',views.search_results, name="search_results"),
-    url('^location/',views.nairobi,name= "nairobi"),
-    url('^location/',views.mombasa,name= "mombasa"),
-    url('^location/',views.upcountry,name= "upcountry"),
-    url('^location/',views.other,name= "other"),
-    url('^location/',views.out,name= "out"),
-
+   
     
 ]
 
--- show_images/views.py
+++ show_images/views.py
@@ -13,41 +13,14 @@ def search_results(request):
         search_term = request.GET.get("image")
 
         searched_images = Image.search_by_category(search_term)
+        print(searched_images)
         message = f"{search_term}"
         
         
 
-        return render(request, 'search.html',{"message":message,"images": searched_images})
+        return render(request, 'search.html',{"message":message,"searched_images": searched_images})
 
     else:
         message = "You haven't searched for any term"
         return render(request, 'search.html',{"message":message})
-
-def nairobi(request):
-    locationterm = 'nairobi'
-    location_images = Image.find_by_location(locationterm)
-    return render(request,'location.html',{'images':location_images }
-    )
-
-def mombasa(request):
-    locationterm = 'mombasa'
-    location_images = Image.find_by_location(locationterm)
-    return render(request,'location.html',{'images':location_images}
-    )
-def upcountry(request):
-    locationterm = 'upcountry'
-    location_images = Image.find_by_location(locationterm)
-    return render(request,'location.html',{'images':location_images}
-    )
-def other(request):
-    locationterm = 'other'
-    location_images = Image.find_by_location(locationterm)
-    return render(request,'location.html',{'images':location_images}
-    )
-def out(request):
-    locationterm = 'out'
-    location_images = Image.find_by_location(locationterm)
-    return render(request,'location.html',{'images':location_images}
-    )
-
 
